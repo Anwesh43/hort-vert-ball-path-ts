85a5d2de0c9a1975e12ba673138dd8582fff3647
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 16:03:46 +0530

    adding vosntants

---
+++ index.ts
@@ -0,0 +1,15 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const parts : number = 3  
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const rFactor : number = 5.9 
+const colors : Array<string> = [
+    "#F44336",
+    "#03A9F4",
+    "#009688",
+    "#3F51B5",
+    "#4CAF50"
+]
+const delay : number = 20 
+const backColor : string = "#BDBDBD"
